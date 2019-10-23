d7c902a932d00250fc14d81fd34c2bf09915797d
Arenas Massa Francisco Horacio <FARENAS@ENTEL.CL>
2019-10-23 20:32:59 -0300

    se sube tercera y final del turotial https://www.youtube.com/watch?v=ccdB22kmR_s

--- main.go
+++ main.go
@@ -38,11 +38,23 @@ func GetPersonEndpoint(w http.ResponseWriter, req *http.Request) {
 }
 
 func CreatePersonEndpoint(w http.ResponseWriter, req *http.Request) {
-
+  params := mux.Vars(req)
+  var person Person
+  _ = json.NewDecoder(req.Body).Decode(&person)
+  person.ID = params["id"]
+  people = append(people, person)
+  json.NewEncoder(w).Encode(person)
 }
 
 func DeletePersonEndpoint(w http.ResponseWriter, req *http.Request) {
-
+  params := mux.Vars(req)
+  for index, item := range people {
+    if item.ID == params["id"] {
+      people = append(people[:index], people[index + 1:]...)
+      break
+    }
+  }
+  json.NewEncoder(w).Encode(people)
 }
 
 func main() {
